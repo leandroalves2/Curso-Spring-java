1c4e28d13fc50d2ecbab0a564f19d7b5e6de64d8
Leandro Alves <leandro_alvesbr@outlook.com>
2024-04-01 20:31:56 -0300

    aula 2

---
+++ src/main/java/com/remedios/leandro/curso/Curso/Spring/Controllers/RemedioController.java
@@ -0,0 +1,17 @@
+package com.remedios.leandro.curso.Curso.Spring.Controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remedios")
+public class RemedioController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json)
+    {
+        System.out.println(json);
+    }
+}
